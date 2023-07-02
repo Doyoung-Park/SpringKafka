e223640b3764fcd68a89ff5ded3c2c00099d829d
Doyoung <doodoo55@gachon.ac.kr>
2023-07-02 11:15:45 +0900

    [feat] 조건에 따라 다른 Topic에 메시지 produce/consume
    클라이언트가 test 라고 보내면 이름이 test2 인 토픽으로 메시지를 produce하고
    
    test가 아니라면 이름이 test1 인 토픽으로 메시지를 produce하는 프로그램

--- src/main/java/practice/kafka/service/KafkaConsumer.java
+++ src/main/java/practice/kafka/service/KafkaConsumer.java
@@ -11,8 +11,15 @@ import java.io.IOException;
 @Service
 public class KafkaConsumer {
 
-    @KafkaListener(topics = "test", groupId = ConsumerConfig.GROUP_ID_CONFIG)
-    public void consume(String message) throws IOException {
+    @KafkaListener(topics = "test1", groupId = ConsumerConfig.GROUP_ID_CONFIG)
+    public void Test1Consume(String message) throws IOException {
+        System.out.println("This is KafkaConsumer.Test1Consume");
+        System.out.println("Consumed message is: " + "'"+message+"'");
+    }
+
+    @KafkaListener(topics = "test2", groupId = ConsumerConfig.GROUP_ID_CONFIG)
+    public void Test2Consume(String message) throws IOException {
+        System.out.println("This is KafkaConsumer.Test2Consume");
         System.out.println("Consumed message is: " + "'"+message+"'");
     }
 }
--- src/main/java/practice/kafka/service/KafkaProducer.java
+++ src/main/java/practice/kafka/service/KafkaProducer.java
@@ -13,14 +13,25 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class KafkaProducer {
 
-    @Value(value = "test")
-    private String topicName;
+    @Value(value = "test1")
+    private String topicName1;
+
+    @Value(value = "test2")
+    private String topicName2;
+
 
     private final KafkaTemplate<String, String> kafkaTemplate;  // 생성한 디폴트 카프카 템플릿을 injection 하는 부분
 
     public void sendMessage(String message) {
         System.out.println("produced message is: " + "'"+message+"'");
-        System.out.println("topic name is: " + "'"+topicName+"'");
-        this.kafkaTemplate.send(topicName, message);
+        if (message.equals("test")) {
+            System.out.println("** message Test = " + message);
+            this.kafkaTemplate.send(topicName2, message);
+        }
+        else{
+            this.kafkaTemplate.send(topicName1, message);
+        }
+
+
     }
 }
